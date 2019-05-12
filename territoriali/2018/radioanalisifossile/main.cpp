5a5e075ec087c9cd67d037a43567cc460ca93d84
mattiagre <46654038+mattiagre@users.noreply.github.com>
2019-05-12 18:02:56 +0200

    Create main.cpp

---
+++ territoriali/2018/radioanalisifossile/main.cpp
@@ -0,0 +1,42 @@
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+vector<int> zone;
+
+int analizza(int from, int to) {
+	bool finito = true;
+	for (int i = from; i < to; i++)
+		if (zone[i] != 0)
+			finito = false;
+	if (finito)
+		return 0;
+	int i = from;
+	for ( ; i < to; i++)
+		if (zone[i] == 0)
+			break;
+	for (int j = from; j < i; j++)
+		--zone[j];
+	if (i == from)
+		return analizza(i + 1, to);
+	else if (i = to)
+		return 1 + analizza(from, to);
+	else return 1 + analizza(from, i) + analizza(i + 1, to);
+}
+
+int main() {
+	int T;
+	ifstream in("input.txt");
+	in >> T;
+	
+	for (int t = 1; t <= T; t++) {
+		int N;
+		in >> N;
+		zone.resize(N);
+		for (auto& z : zone)
+			in >> z;
+		out << "Case #" << t << ": " << analizza(0, N) << endl;
+	}
+	
+}
