24b2362b51abe925566548ff7e3a037eeb88bcd8
mattiagre <46654038+mattiagre@users.noreply.github.com>
2019-05-12 18:12:40 +0200

    Create main.cpp

---
+++ territoriali/2019/download/main.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	int T;
+	ifstream in("input.txt");
+  ofstream out("output.txt");
+	in >> T;
+	for (int t = 1; t <= T; t++) {
+		int N, F, C;
+		in >> N >> F >> C;
+		int nFilmati, nCanzoni;
+		nFilmati = N / F;
+		N %= F;
+		nCanzoni = N / C;
+		out << "Case #" << t << ": " << nFilmati << ' ' << nCanzoni;
+	}
+}
